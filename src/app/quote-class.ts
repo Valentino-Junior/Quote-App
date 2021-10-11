632f756de28cd2f4d42607ee17d8b763b945a675
valentine-ochieng <valentine.ochieng@student.moringaschool.com>
2021-10-11 12:11:14 +0300

    imported quote class to quote details component.ts

--- src/app/quote-class.ts
+++ src/app/quote-class.ts
@@ -3,5 +3,5 @@ export class QuoteClass {
     showQuotes!:boolean;
     constructor (public id: number, public author:string, public quote: string, public postedBy: string, public postDate: Date) {
         this.showQuotes=false
-
+    }
 }
