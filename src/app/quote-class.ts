817b6efa49a88d4f53b34e5bc6cb052fdf931c2c
valentine-ochieng <valentine.ochieng@student.moringaschool.com>
2021-10-11 11:38:45 +0300

    Defined class values

--- src/app/quote-class.ts
+++ src/app/quote-class.ts
@@ -1,2 +1,7 @@
 export class QuoteClass {
+
+    showQuotes!:boolean;
+    constructor (public id: number, public author:string, public quote: string, public postedBy: string, public postDate: Date) {
+        this.showQuotes=false
+
 }
