884c610c52a8ac69273f3a780027886cbb81fa1f
valentine-ochieng <valentine.ochieng@student.moringaschool.com>
2021-10-12 01:23:07 +0300

    Added highlight directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { QuoteFormComponent } from './quote-form/quote-form.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 import { DurationCountPipe } from './duration-count.pipe';
 import { FormsModule } from '@angular/forms';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { FormsModule } from '@angular/forms';
     QuoteComponent,
     QuoteFormComponent,
     QuoteDetailsComponent,
-    DurationCountPipe
+    DurationCountPipe,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
